664e598ff281067b93630b1989875417311200c0
Siyabonga Shabangu <siyabonga23shabangu@gmail.com>
2021-12-28 16:26:09 +0200

    retouched nav and changed logo

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
-import logo from '../../assets/logo.svg';
+import logo2 from '../../assets/logo2.png';
 import './navbar.css';
 
 const Menu = () => (
@@ -19,14 +19,14 @@ const Navbar = () => {
     return (
         <div className="gpt3__navbar">
             <div className="gpt3__navbar-links-logo">
-                <img src={logo} alt="logo" />
+                <h2 className="gradient__text">LOGO</h2>
             </div>
             <div className="gpt3__navbar-links">
                 <div className="gpt3__navbar-links_container  tracking-in-expand ">
                     <Menu />
                 </div>
             </div>
-            <div className="gpt3__navbar-sign tracking-in-expand text-focus-in">
+            <div className="gpt3__navbar-sign ">
                 <p>Sign in</p>
                 <button type="button">Sign up</button>
             </div>
