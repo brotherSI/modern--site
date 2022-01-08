214b642055f1a9256a1a531e79a71e610f2cc464
Siyabonga Shabangu <siyabonga23shabangu@gmail.com>
2022-01-08 23:13:36 +0200

    added to blog

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Article } from '../../components';
-import { blog01, blog02, blog03, blog04, blog05 } from './imports';
+import { blog1, blog02, blog03, blog04, blog05 } from './imports';
 import './blog.css';
 
 const Blog = () => {
@@ -11,7 +11,7 @@ const Blog = () => {
             </div>
             <div className="gpt3__blog-container">
                 <div className="gpt3__blog-container_groupA">
-                    <Article imgUrl={blog01} date="Sep 29, 2021" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+                    <Article imgUrl={blog1} date="Sep 29, 2021" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
                 </div>
                 <div className="gpt3__blog-container_groupB">
                     <Article imgUrl={blog02} date="Sep 29, 2021" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
